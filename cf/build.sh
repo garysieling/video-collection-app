6ee06b3237e6ee96c6b16567b85eaac824ddcdf0
Gary Sieling <gary@garysieling.com>
2019-06-07 22:47:57 -0400

    Add cloudfront for ssl

--- cf/build.sh
+++ cf/build.sh
@@ -1,15 +1,17 @@
 set -e
-
+set -o xtrace
 cd ..
-gatsby build 
+#gatsby build 
 cd cf
 
 STACK_NAME=video-player
 REGION=us-east-1
 
+PARAMETERS="--parameters ParameterKey=DomainName,ParameterValue=videoclips.garysieling.com"
+
 aws cloudformation validate-template --template-body file://cloudformation.yaml || exit
-#aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://cloudformation.yaml 
-#aws cloudformation update-stack --stack-name $STACK_NAME --template-body file://cloudformation.yaml || exit
+#aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://cloudformation.yaml $PARAMETERS
+aws cloudformation update-stack --stack-name $STACK_NAME $PARAMETERS --template-body file://cloudformation.yaml || exit
 
 S3_BUCKET=$(aws cloudformation list-stack-resources --stack-name $STACK_NAME \
   | jq '.StackResourceSummaries | .[] | select(.LogicalResourceId | inside("WebsiteBucket"))' \
