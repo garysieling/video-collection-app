585ec26a0115cd0f12c22c88b9883f192a29f688
Gary Sieling <gary@garysieling.com>
2019-07-15 22:47:39 -0400

    Add more resources

--- cf/build.sh
+++ cf/build.sh
@@ -2,7 +2,7 @@ set -e
 set -o xtrace
 cd ..
 
-gatsby build 
+#gatsby build 
 
 cd cf
 
@@ -11,12 +11,28 @@ REGION=us-east-1
 UNAUTH_ROLE=$1
 AUTH_ROLE=$2
 
+if [ -z $UNAUTH_ROLE ]; then 
+  UNAUTH_ROLE="Cognito_Auth0Unauth_Role"
+fi
+
+if [ -z $AUTH_ROLE ]; then 
+  AUTH_ROLE="Cognito_Auth0Auth_Role"
+fi
+
+if [ -z $PASSWORD ]; then 
+  PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+fi
+
 # ParameterKey=DomainName,ParameterValue=videoclips.garysieling.com
-PARAMETERS="--parameters ParameterKey=UnauthenticatedUserRole,ParameterValue=$UNAUTH_ROLE ParameterKey=AuthenticatedUserRole,ParameterValue=$AUTH_ROLE"
+PARAMETERS="--parameters ParameterKey=UnauthenticatedUserRole,ParameterValue=$UNAUTH_ROLE ParameterKey=AuthenticatedUserRole,ParameterValue=$AUTH_ROLE ParameterKey=DatabasePassword,ParameterValue=$PASSWORD"
 
 aws cloudformation validate-template --template-body file://cloudformation.yaml || exit
 #aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://cloudformation.yaml $PARAMETERS
-aws cloudformation update-stack --stack-name $STACK_NAME $PARAMETERS --template-body file://cloudformation.yaml || exit
+aws cloudformation update-stack --stack-name $STACK_NAME \
+  $PARAMETERS \
+  --template-body file://cloudformation.yaml || exit
+
+cfn-tail --region $REGION $STACK_NAME
 
 S3_BUCKET=$(aws cloudformation list-stack-resources --stack-name $STACK_NAME \
   | jq '.StackResourceSummaries | .[] | select(.LogicalResourceId | test("^WebsiteBucket$"))' \
